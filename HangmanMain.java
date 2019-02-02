0360f11b17925faf6439f7970f7d40dbb3d7e80c
MarcusGHill <mg223hh@student.lnu.se>
2019-02-02 23:12:36 +0100

    its not working, trying again

---
+++ HangmanMain.java
@@ -0,0 +1,15 @@
+package HangmanPackage;
+
+asdasd
+
+public class HangmanMain {
+
+	
+	public static void main (String[] args) {
+		ReadWordsFromWeb rwfw = new ReadWordsFromWeb();
+		HangmanLogic hl = new HangmanLogic();
+		rwfw.ReadInput();
+		hl.printHangMan();
+System.out.println("test");
+	}
+}
---
+++ ReadWordsFromWeb.java
@@ -0,0 +1,11 @@
+package HangmanPackage;
+
+public class ReadWordsFromWeb {
+ 
+	
+	public  void ReadInput () {
+		System.out.println("Reading web input here");
+		System.out.println("test");
+	}
+}
+
